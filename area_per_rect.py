706933ac0b8bc19007b52b70d8590b2ec9a82af8
Lily Carroll <lily.carroll@stu.ocsb.ca>
2023-09-20 02:35:01 +0000

    Added area_per_rect.py and basic_math.py files

---
+++ area_per_rect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created By: Lily Carroll
+# Date: Sept/19/2023
+# This program calculates the area and perimeter of a 7cm x 4cm rectangle.
+
+
+def main():
+    print("For a rectangle with a length of 7 cm and a width of 4 cm:")
+    print()
+    print("The area is = {} cm^2".format(7 * 4))
+    print("The perimeter is = {} cm".format(2 * (7 + 4)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Created By: Lily Carroll
+# Date: Sept/19/2023
+# Write what this program does basic math.
+
+
+def main():
+    print("9 + 3 = {}".format(9 + 3))  # Addition
+    print("203 - 74 = {}".format(203 - 74))  # Subtraction
+    print("6*7 = {}".format(6 * 7))  # Multiplication
+    print("77 / 2 = {}".format(77 / 2))  # Division
+    print("25 / 3 = {}".format(25 / 3))  # Division with decimal answer
+    print("11 + 8 * 4 = {}".format(11 + 8 * 4))  # Multiplication and addition
+    print("9 - 2 / 2 = {}".format(9 - 2 / 2))  # Division and subtraction
+    print("7^9 = {}".format(7 ^ 9))  # bitwise operator (not exponent)
+    print("3**3 = {}".format(3**3))  # Exponent
+    print("16**0.5 = {}".format(16**0.5))  # Square root
+
+
+if __name__ == "__main__":
+    main()
